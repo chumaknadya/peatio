6a76daa7600af348c88c0b7f712ec66bd42510fb
Chumak Nadiia <chumaknadya19@gmail.com>
2020-04-10 11:17:38 +0300

    Bitgo

---
+++ lib/peatio/bitgo/client.rb
@@ -0,0 +1,52 @@
+module Bitgo
+  class Client
+    Error = Class.new(StandardError)
+
+    class ConnectionError < Error; end
+
+    class ResponseError < Error
+      def initialize(msg)
+        super "#{msg}"
+      end
+    end
+
+    def initialize(endpoint, access_token, idle_timeout: 5)
+      @endpoint = URI.parse(endpoint)
+      @access_token = access_token
+      @idle_timeout = idle_timeout
+    end
+
+    def rest_api(verb, path, data = nil)
+      args = [@endpoint.to_s + path]
+
+      if data
+        if verb.in?(%i[ post put patch ])
+          args << data.compact.to_json
+          args << { 'Content-Type' => 'application/json' }
+        else
+          args << data.compact
+          args << {}
+        end
+      else
+        args << nil
+        args << {}
+      end
+
+      args.last['Accept']        = 'application/json'
+      args.last['Authorization'] = 'Bearer ' + @access_token
+
+      response = Faraday.send(verb, *args)
+      response.assert_success!
+      response = JSON.parse(response.body)
+      response['error'].tap { |error| raise ResponseError.new(error) if error }
+      response
+    rescue Faraday::Error => e
+      
+      binding.pry
+      
+      raise ConnectionError, e
+    rescue StandardError => e
+      raise Error, e
+    end
+  end
+end
---
+++ lib/peatio/bitgo/wallet.rb
@@ -0,0 +1,132 @@
+module Bitgo
+  class Wallet < Peatio::Wallet::Abstract
+
+    def initialize(settings = {})
+      @settings = settings
+    end
+
+    def configure(settings = {})
+      # Clean client state during configure.
+      @client = nil
+
+      @settings.merge!(settings.slice(*SUPPORTED_SETTINGS))
+
+      @wallet = @settings.fetch(:wallet) do
+        raise Peatio::Wallet::MissingSettingError, :wallet
+      end.slice(:uri, :address, :secret, :access_token, :wallet_id, :testnet)
+
+      @currency = @settings.fetch(:currency) do
+        raise Peatio::Wallet::MissingSettingError, :currency
+      end.slice(:id, :base_factor, :code, :options)
+    end
+
+    def create_address!(options = {})
+      if options[:bitgo_address_id].present?
+        response = client.rest_api(:get, '/wallet/' + wallet_id + '/address/' + options[:bitgo_address_id])
+        { address: response['address'] }
+      else
+        response = client.rest_api(:post, 'ff/wallet/' + wallet_id + '/address')
+        { address: response['address'], secret: bitgo_wallet_passphrase, details: { bitgo_address_id: response['id'] }}
+      end
+    rescue Bitgo::Client::Error => e
+      raise Peatio::Wallet::ClientError, e
+    end
+
+    def create_transaction!(transaction, options = {})
+      currency_options = @currency.fetch(:options).slice(:gas_limit, :gas_price)
+
+      if currency_options[:gas_limit].present? && currency_options[:gas_price].present?
+        options.merge!(currency_options)
+        create_eth_transaction(transaction, options)
+      else
+        if options[:subtract_fee].to_s == 'true'
+          fee = build_raw_transaction(transaction) if options[:subtract_fee].to_s == 'true'
+          fee.dig('feeInfo','fee').present? ? fee = fee.dig('feeInfo','fee') : fee = fee.dig('txInfo','Fee')
+        end
+
+        fee.present? ? fee = fee.to_i : fee = 0
+
+        txid = client.rest_api(:post, '/wallet/' + wallet_id + '/sendcoins', {
+                                    address: transaction.to_address.to_s,
+                                    amount: (convert_to_base_unit(transaction.amount) - fee).to_s,
+                                    walletPassphrase: bitgo_wallet_passphrase
+        }.compact).fetch('txid')
+
+        transaction.hash = normalize_txid(txid)
+        transaction
+      end
+    rescue Bitgo::Client::Error => e
+      raise Peatio::Wallet::ClientError, e
+    end
+
+    def build_raw_transaction(transaction)
+      client.rest_api(:post, '/wallet/' + wallet_id + '/tx/build', {
+        recipients: [{
+          address: transaction.to_address,
+          amount: convert_to_base_unit(transaction.amount).to_s
+        }]
+      }.compact)
+    end
+
+    def create_eth_transaction(transaction, options = {})
+      amount = convert_to_base_unit(transaction.amount)
+      amount -= options.fetch(:gas_limit).to_i * options.fetch(:gas_price).to_i if options.dig(:subtract_fee)
+
+      txid = client.rest_api(:post, '/wallet/' + wallet_id + '/sendcoins', {
+          address: transaction.to_address.to_s,
+          amount: amount.to_s,
+          walletPassphrase: bitgo_wallet_passphrase,
+          gas: options.fetch(:gas_limit).to_i,
+          gasPrice: options.fetch(:gas_price).to_i
+      }.compact).fetch('txid')
+
+      transaction.hash = normalize_txid(txid)
+      transaction
+    end
+
+    def load_balance!
+      response = client.rest_api(:get, "/wallet/#{wallet_id}")
+      convert_from_base_unit(response.fetch('balanceString'))
+    rescue Bitgo::Client::Error => e
+      raise Peatio::Wallet::ClientError, e
+    end
+
+    private
+
+    def client
+      currency_id = @currency.fetch(:id) { raise Peatio::Wallet::MissingSettingError, :id }
+      uri = @wallet.fetch(:uri) { raise Peatio::Wallet::MissingSettingError, :uri }
+      access_token = @wallet.fetch(:access_token) { raise Peatio::Wallet::MissingSettingError, :access_token }
+
+      currency_code_prefix = @wallet.fetch(:testnet) ? 't' : ''
+      uri = uri.gsub(/\/+\z/, '') + '/' + currency_code_prefix + currency_id
+      @client ||= Client.new(uri, access_token, idle_timeout: 1)
+    end
+
+    def bitgo_wallet_passphrase
+      @wallet.fetch(:secret)
+    end
+
+    def wallet_id
+      @wallet.fetch(:wallet_id)
+    end
+
+    def normalize_txid(txid)
+      txid.downcase
+    end
+
+    def convert_from_base_unit(value)
+      value.to_d / @currency.fetch(:base_factor)
+    end
+
+    def convert_to_base_unit(value)
+      x = value.to_d * @currency.fetch(:base_factor)
+      unless (x % 1).zero?
+        raise Peatio::WalletClient::Error,
+            "Failed to convert value to base (smallest) unit because it exceeds the maximum precision: " \
+            "#{value.to_d} - #{x.to_d} must be equal to zero."
+      end
+      x.to_i
+    end
+  end
+end
